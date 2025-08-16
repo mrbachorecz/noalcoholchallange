14e415e98c7464b6bcb2c09d16e14687e9ec2b8e
mrbachorecz <bachoreczm@gmail.com>
2025-08-16 17:28:54 +0200

    hamburger menu init

--- app/src/main/java/com/mrbachorecz/noalcohol/MainCardActivity.kt
+++ app/src/main/java/com/mrbachorecz/noalcohol/MainCardActivity.kt
@@ -4,15 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainCardActivity : ComponentActivity() {
 
@@ -23,10 +40,10 @@ class MainCardActivity : ComponentActivity() {
                 val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
                 val storedDate = prefs.getString(STORED_DATE_KEY, "")
 
-                if (storedDate != null) {
+                if (storedDate != null && storedDate.isNotEmpty()) {
                     MainCardScreen(
                         storedDate = storedDate,
-                        onReset =  {
+                        onReset = {
                             startActivity(Intent(this, DatePickerActivity::class.java))
                             finish()
                         },
@@ -45,55 +62,61 @@ fun MainCardScreen(
     onReset: () -> Unit,
     onSettingsClick: () -> Unit
 ) {
-    var menuExpanded by remember { mutableStateOf(false) }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    if (storedDate != null) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {},
-                    navigationIcon = {
-                        IconButton(onClick = { menuExpanded = true }) {
-                            Icon(Icons.Filled.Menu, contentDescription = "Menu")
-                        }
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = Color.Transparent
-                    ),
-                    actions = {}
-                )
-                DropdownMenu(
-                    expanded = menuExpanded,
-                    onDismissRequest = { menuExpanded = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text("Settings") },
+    if (storedDate != null && storedDate.isNotEmpty()) {
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    NavigationDrawerItem(
+                        label = { Text("Settings") },
+                        selected = false,
                         onClick = {
-                            menuExpanded = false
+                            scope.launch { drawerState.close() }
                             onSettingsClick()
                         }
                     )
                 }
             },
-            bottomBar = {
+            drawerState = drawerState,
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {},
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.Transparent
+                        ),
+                        actions = {}
+                    )
+                },
+                bottomBar = {
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .navigationBarsPadding(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        SubmitButton(text = "Reset", onSubmit = onReset)
+                    }
+                },
+                containerColor = Color.Transparent
+            ) { innerPadding ->
                 Box(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
                     contentAlignment = Alignment.Center
                 ) {
-                    SubmitButton(text = "Reset", onSubmit = onReset)
+                    ElevatedCardWithContent(calculateDaysPassed(storedDate))
                 }
-            },
-            containerColor = Color.Transparent
-        ) { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                contentAlignment = Alignment.Center
-            ) {
-                ElevatedCardWithContent(calculateDaysPassed(storedDate))
             }
         }
     }
