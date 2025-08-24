ecf9a9eb8710289e07fe3063ff2f0fc2cdd88752
mrbachorecz <bachoreczm@gmail.com>
2025-08-24 15:06:01 +0200

    refactor days calculator

--- app/src/main/java/com/mrbachorecz/noalcohol/maincard/DaysCalculator.kt
+++ app/src/main/java/com/mrbachorecz/noalcohol/maincard/DaysCalculator.kt
@@ -6,16 +6,25 @@ import java.time.temporal.ChronoUnit
 
 object DaysCalculator {
     fun calculateDaysPassedMessage(storedDate: String): String {
+        if (storedDate.isEmpty()) return "No Alcohol: 0 day"
         val formatter = DateTimeFormatter.ISO_LOCAL_DATE
         val startDate = LocalDate.parse(storedDate, formatter)
-        val daysPassed = ChronoUnit.DAYS.between(startDate, LocalDate.now())
-        return "No Alcohol: $daysPassed days"
+        val daysPassed = calculateDaysSinceThen(startDate)
+        val unit = if (daysPassed == 1) "day" else "days"
+        return "No Alcohol: $daysPassed $unit"
     }
 
     fun calculateDaysPassed(storedDate: String): Int {
         if (storedDate.isEmpty()) return 0
         val formatter = DateTimeFormatter.ISO_LOCAL_DATE
         val startDate = LocalDate.parse(storedDate, formatter)
-        return ChronoUnit.DAYS.between(startDate, LocalDate.now()).toInt()
+        return calculateDaysSinceThen(startDate)
+    }
+
+    private fun calculateDaysSinceThen(startDate: LocalDate?): Int {
+        if (startDate == null) return 0
+        val days = ChronoUnit.DAYS.between(startDate, LocalDate.now()).toInt()
+        if (days < 0) return 0
+        return days
     }
 }
