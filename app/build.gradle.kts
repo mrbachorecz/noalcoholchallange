09e6c4417e54e91d2cb799ee20a3df6d54652b73
mrbachorecz <bachoreczm@gmail.com>
2025-09-09 19:59:47 +0200

    upgrade version to v1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mrbachorecz.noalcohol"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
