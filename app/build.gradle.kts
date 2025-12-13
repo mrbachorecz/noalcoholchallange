736577d1ef992156947324552b10d4763d63825b
mrbachorecz <bachoreczm@gmail.com>
2025-12-13 10:08:15 +0100

    upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mrbachorecz.noalcohol"
         minSdk = 29
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
